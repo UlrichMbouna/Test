336e1b73ea08310b863d9148934d833a16a92216
UlrichMbouna <koudjouulrich@gmail.com>
2024-08-27 14:25:16 +0200

    liaison entres les 02 services

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,12 @@
-# Utilisez une image Maven pour construire le projet
-FROM maven:3.9.8-amazoncorretto-17-al2023 AS build
-
-# Définir le répertoire de travail
+# Étape 1: Construction
+FROM maven:3.8.5-openjdk-17 AS build
 WORKDIR /app
-
-# Copier le fichier pom.xml dans le conteneur
 COPY pom.xml .
-
-# Copier les sources du projet dans le conteneur
 COPY src ./src
+RUN mvn clean package
 
-# Construire le projet
-RUN mvn clean package -DskipTests
-
-# Utiliser une image Java pour exécuter l'application
-FROM openjdk:24-slim-bullseye
-
-# Copier le fichier JAR construit dans l'étape précédente
-COPY --from=build /app/target/mon-app.jar /app/mon-app.jar
-
-# Définir le point d'entrée de l'application
-ENTRYPOINT ["java", "-jar", "/app/mon-app.jar"]
+# Étape 2: Création de l'image finale
+FROM eclipse-temurin:17-jdk-alpine
+VOLUME /tmp
+COPY --from=build /app/target/csvforjson-0.0.1-SNAPSHOT.jar /app/csvforjson.jar
+ENTRYPOINT ["java", "-jar", "/app/csvforjson.jar"]
--- src/main/java/ulrich_tech/csvforjson/Controllers/CsvToJsonController.java
+++ src/main/java/ulrich_tech/csvforjson/Controllers/CsvToJsonController.java
@@ -3,6 +3,8 @@ package ulrich_tech.csvforjson.Controllers;
 import ulrich_tech.csvforjson.Services.CsvToJsonService;
 import com.opencsv.exceptions.CsvException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,5 +20,17 @@ public class CsvToJsonController {
     @PostMapping("/CsvForJson")
     public String convertCsvToJson(@RequestParam("file") MultipartFile file) throws IOException, CsvException {
         return csvToJsonService.convertCsvToJson(file);
+    }
+
+     @PostMapping("/JsonForCsv")
+    public ResponseEntity<String> convertJsonToCsv(@RequestParam("file") MultipartFile file) throws IOException {
+        String json = new String(file.getBytes());
+        String csv = csvToJsonService.convertJsonToCsv(json);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, "text/csv");
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"output.csv\"");
+
+        return ResponseEntity.ok().headers(headers).body(csv);
     }
 }
